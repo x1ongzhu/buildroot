ce85d3f9205f473845d092320f1516c3575f0d4e
Michel-FK <michel.stempin@funkey-project.com>
2020-12-25 15:31:25 +0100

    bump tinyalsa to latest version
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- package/tinyalsa/tinyalsa.mk
+++ package/tinyalsa/tinyalsa.mk
@@ -4,30 +4,11 @@
 #
 ################################################################################
 
-TINYALSA_VERSION = 1.1.1
-TINYALSA_SITE = $(call github,tinyalsa,tinyalsa,$(TINYALSA_VERSION))
+TINYALSA_VERSION = 30d4711
+FCEUX_SITE_METHOD = git
+FCEUX_SITE = https://github.com/FunKey-Project/tinyalsa.git
 TINYALSA_LICENSE = BSD-3-Clause
 TINYALSA_LICENSE_FILES = NOTICE
 TINYALSA_INSTALL_STAGING = YES
 
-define TINYALSA_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D)
-endef
-
-define TINYALSA_INSTALL_STAGING_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) \
-		-C $(@D) \
-		PREFIX="/usr" \
-		CROSS_COMPILE="$(TARGET_CROSS)" \
-		DESTDIR="$(STAGING_DIR)" install
-endef
-
-define TINYALSA_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) \
-		-C $(@D) \
-		PREFIX="/usr" \
-		CROSS_COMPILE="$(TARGET_CROSS)" \
-		DESTDIR="$(TARGET_DIR)" install
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
