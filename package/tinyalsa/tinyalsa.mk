dcaa05efeac452685c855d957363054d29a66d5c
Michel-FK <michel.stempin@funkey-project.com>
2020-12-25 17:57:27 +0100

    bump to latest tinyalsa
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- package/tinyalsa/tinyalsa.mk
+++ package/tinyalsa/tinyalsa.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 TINYALSA_VERSION = 30d4711
-FCEUX_SITE_METHOD = git
-FCEUX_SITE = https://github.com/FunKey-Project/tinyalsa.git
+TINYALSA_SITE_METHOD = git
+TINYALSA_SITE = https://github.com/FunKey-Project/tinyalsa.git
 TINYALSA_LICENSE = BSD-3-Clause
 TINYALSA_LICENSE_FILES = NOTICE
 TINYALSA_INSTALL_STAGING = YES
